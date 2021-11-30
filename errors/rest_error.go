783b633898d26053a44c52090130e1485c6bd2fc
alireza <a.khorasany@gmail.com>
2021-12-01 02:08:27 +0330

    add new method status

--- errors/rest_error.go
+++ errors/rest_error.go
@@ -14,3 +14,10 @@ func (e *RestErr) NewStatusNotAcceptable(message string) *RestErr {
 	e.Error = "not_accepted"
 	return e
 }
+
+func (e *RestErr) NewStatusNoContent(message string) *RestErr {
+	e.Message = message
+	e.Status = http.StatusNoContent
+	e.Error = "empty_content"
+	return e
+}
